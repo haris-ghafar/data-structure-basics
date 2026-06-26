5b65f02b08e42e76647d62d0df20aace54bb805e
Haris Ghafar <harisbinghafar@gmail.com>
2026-06-26 22:24:24 +0500

    adding stack and queue example

--- main.py
+++ main.py
@@ -35,5 +35,91 @@ student["age"] = 21  # updating value for key "age"
 
 student["city"] = "Rahim Yar Khan"  # adding new key-value pair
 
-for key in student:  # iterating through dictionary
-    print(key, ":", student[key])
+for key in student.keys():  # iterating through dictionary to get keys
+    print(key)
+
+
+for value in student.values():  # iterating through dictionary to get values
+    print(value)
+
+
+for key, value in student.items():  # iterating through dictionary to get key-value pairs
+    print(key, ":", value)
+
+
+student.pop("grade")  # removing key-value pair with key "grade"
+
+
+# TIME COMPLEXITY
+"""
+Dictionary
+Lookup      O(1)
+Insert      O(1)
+Delete      O(1)
+
+"""
+
+
+#STACK EXAMPLE (LIFO)
+
+stack = []
+
+#push operation
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+
+peek = stack[-1]  # accessing the top element of the stack
+print("Top element of stack is:", peek)
+
+
+
+popped_element = stack.pop()  # removing the top/last element of the stack
+print("Popped element from stack is:", popped_element)
+
+
+"""
+Time Complexity
+Stack
+Push        O(1)
+Pop         O(1)
+Peek        O(1)
+"""
+
+
+
+
+# QUEUE EXAMPLE (FIFO)
+
+from collections import deque
+
+# Creating an empty queue
+queue = deque()
+
+# Enqueue (Add elements)
+queue.append("Alice")
+queue.append("Bob")
+queue.append("Charlie")
+
+print("Queue after Enqueue Operations:")
+print(queue)
+
+# Peek (View the front element)
+print("\nFront Element:", queue[0])
+
+# Dequeue (Remove the front element)
+removed_item = queue.popleft()
+print("\nRemoved Item:", removed_item)
+
+
+
+
+
+"""
+Time Complexity
+Queue
+Enqueue     O(1)
+Dequeue     O(1)
+Peek        O(1)
+"""
